4fbbfa2dd0172e5c7639a3254ee002f91c76e068
Aleksandra <a.chekichevska@axeltra.com>
2025-05-12 09:44:48 +0200

    Initial commit

---
+++ MoviesApp.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Aleksandra Axeltra on 12.5.25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
