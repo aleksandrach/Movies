cb98a17d8ed790796c4f6486f29c166e88cabff7
Aleksandra <a.chekichevska@axeltra.com>
2025-05-12 10:03:45 +0200

    Add initial Trending Movies ViewModel

---
+++ TrendingMoviesList/ViewModel/TrendingMoviesListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TrendingMoviesListViewModel.swift
+//  Movies
+//
+//  Created by Aleksandra Axeltra on 12.5.25.
+//
+
+import Foundation
+
+class TrendingMoviesListViewModel: ObservableObject {
+    @Published var movies: [Movie] = []
+    
+    
+}
